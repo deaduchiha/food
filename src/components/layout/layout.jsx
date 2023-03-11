ebc262fcee39cee9441e8405a1878e4860c3ac09
Alireza Nikzad <106245596+deaduchiha@users.noreply.github.com>
2023-03-11 14:51:57 +0330

    install chakra icons

---
+++ src/components/layout/layout.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import {
+  Box,
+  Flex,
+  Text,
+  IconButton,
+  Button,
+  Stack,
+  Collapse,
+  Icon,
+} from "@chakra-ui/react";
+
+import {
+  HamburgerIcon,
+  CloseIcon,
+  ChevronDownIcon,
+  ChevronRightIcon,
+} from "@chakra-ui/icon";
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <h1>layout</h1>
+    </div>
+  );
+};
+export default Layout;
